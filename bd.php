a443def2ae99fff8e1266a9c4b51e6b3198ba361
strelchuk18 <strelchuk18@gmail.com>
2019-10-28 16:25:06 +0200

    Added fix in code

--- bd.php
+++ bd.php
@@ -16,9 +16,9 @@ $fn = $_POST['first_name'];
 $ln = $_POST['last_name'];
 $email = $_POST['email'];
 
- echo ($fn) . "<br />"; 
- echo ($ln) . "<br />";
-echo ($email) . "<br />";
+	echo ($fn) . "<br />"; 
+	echo ($ln) . "<br />";
+	echo ($email) . "<br />";
 
 $result = mysqli_query($connection, "INSERT INTO `users`(`first_name`, `last_name`, `email` ) VALUES ('$fn','$ln', '$email')");
 if ($result) {
@@ -27,10 +27,7 @@ if ($result) {
 else {
     echo "Произошла ошибка, пожалуйста повторите попытку.";
 } 
-?>
-<?php
-header("Location: http://new.loc/"); /* Перенаправление браузера */
 
-/* Убедиться, что код ниже не выполнится после перенаправления .*/
+header("Location: http://new.loc/");
 exit;
 ?> 
